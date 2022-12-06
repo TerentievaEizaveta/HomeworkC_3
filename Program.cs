1629c288ed31205b217f2a87d011b48ada665658
User <elizaveta.terentieva1993@yandex.ru>
2022-12-07 00:15:40 +0300

    Решено 2 задачи (2 и 3)

--- Program.cs
+++ Program.cs
@@ -2,13 +2,52 @@
 /* Задача 19
 Напишите программу, которая принимает на вход пятизначное число и 
 проверяет, является ли оно палиндромом.*/
+Console.Clear();
+
 
 /*Задача 21
 Напишите программу, которая принимает на вход координаты двух точек 
 и находит расстояние между ними в 3D пространстве.*/
+/*Console.Clear();
+double Dist(double xA, double yA,double zA, double xB, double yB,double zB)
+{
+    double dist=Math.Sqrt(Math.Pow(xB-xA,2)+Math.Pow(yB-yA,2)+Math.Pow(zB-zA,2));
+    return Math.Round(dist,2);
+}
+Console.Write("Input a coordinate x of point A:  ");
+double xA=Convert.ToDouble(Console.ReadLine());
+Console.Write("Input a coordinate y of point A:  ");
+double yA=Convert.ToDouble(Console.ReadLine());
+Console.Write("Input a coordinate z of point A:  ");
+double zA=Convert.ToDouble(Console.ReadLine());
 
+Console.Write("Input a coordinate x of point B:  ");
+double xB=Convert.ToDouble(Console.ReadLine());
+Console.Write("Input a coordinate y of point B:  ");
+double yB=Convert.ToDouble(Console.ReadLine());
+Console.Write("Input a coordinate z of point B:  ");
+double zB=Convert.ToDouble(Console.ReadLine());
+
+double distance=Dist(xA, yA,zA, xB, yB,zB);
+Console.WriteLine($"Distance  between points A and B are equal  {distance}");
+*/
 /*Задача 23
 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
 
+/*
 Console.Clear();
-
+void CubesOfNumbers(int num)
+{
+    int count=1;
+    Console.WriteLine("Number     Number^3  ");
+    while (count<=num)
+    {
+        int coub=Convert.ToInt32(Math.Pow(count,3));
+        Console.WriteLine($"   {count}        {coub}");
+        count ++;
+    }
+}
+Console.WriteLine("Input a number");
+int num=Convert.ToInt32(Console.ReadLine());
+CubesOfNumbers(num);
+*/
